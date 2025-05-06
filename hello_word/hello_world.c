141ea870f9f83e41b40635e539f871cb3e016b1f
Mahafujmr <mrtuhin444@gmail.com>
2025-05-06 23:56:54 +0530

    classic beginers programme hello world

--- hello_word/hello_world.c
+++ hello_word/hello_world.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
-    printf("hello tuhin");
+    printf("Hello C programming");
     return 0;
 }
--- hellowworld.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Hello World C Programming"); 
-    return 0;
-}
