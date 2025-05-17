531e37fdf0ace1ff65a889ae456268b6b22c1b95
Mahafujmr <mrtuhin444@gmail.com>
2025-05-17 11:43:40 +0530

    user imput from terminal

--- data_type/other_type_number.c
+++ data_type/other_type_number.c
@@ -5,7 +5,8 @@ int main()
     float rollNumber = 20;
     double amnoutNumber = 100.022;
     char ch = 'a';
-    printf("This is a Floating Number %f\n", rollNumber);
+    printf("This is a Floating Number %.1f\n", rollNumber);
     printf("This is a amount Number %lf\n", amnoutNumber);
     printf("This is a character Name %c\n", ch);
+    return 0;
 }
---
+++ data_type/user_input.c
@@ -0,0 +1,9 @@
+// write a programme that takes and int, and print that number
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Please Enter the Number = ");
+    scanf("%d",&number);
+    printf("Print this number is = %d",number);
+}
