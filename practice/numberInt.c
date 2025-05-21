a9c816505e76110ef8391f21a99dd8a327f4e01a
Mahafujmr <mrtuhin444@gmail.com>
2025-05-22 01:33:25 +0530

    int roll number print

---
+++ practice/numberInt.c
@@ -0,0 +1,9 @@
+// write a programme and prints int number 
+#include <stdio.h>
+int main()
+{
+    int rollNumber = 10;
+    int rollSchool = 100;
+    printf("My Roll Number is %d\n",rollNumber);
+    printf("My Friend Roll Number is %d",rollSchool);
+}
