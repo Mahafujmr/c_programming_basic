94cef2a93e80dfd25a6634d502dc4075fe68d475
Mahafujmr <mrtuhin444@gmail.com>
2025-05-23 00:16:05 +0530

    code clear comment use

--- practice/helloTuhin.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int roll =100;
-    printf("MY Roll Number is %d",roll);
-}
---
+++ practice/int_number_part/intNumber.c
@@ -0,0 +1,9 @@
+// write a programme prints int number 
+#include<stdio.h>
+int main()
+{
+    int roll= 100;
+    int subjectCode =1300;
+    printf("My Roll Number is %d,\n and My Subject Code %d",roll,subjectCode);
+    return 0;
+}
