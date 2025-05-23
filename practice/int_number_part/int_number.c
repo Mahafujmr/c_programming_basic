d659939ef032fe673f4e98e37f09ea21ea52a729
Mahafujmr <mrtuhin444@gmail.com>
2025-05-24 01:39:47 +0530

    integer value

--- practice/int_number_part/int_number.c
+++
@@ -1,8 +0,0 @@
-// write a programme that print integer number
-#include<stdio.h>
-int main()
-{
-    int number =50;
-    printf("This number is %d,",number);
-    return 0;
-}
---
+++ practice/int_value.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int number =1000;
+    int code =200;
+    printf("This is  %d, \n this also number is %d",number,code);
+}
