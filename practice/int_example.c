bb4b8bfb455c7a0a3c4643aaa0a89134002caf0a
Mahafujmr <mrtuhin444@gmail.com>
2025-05-24 23:23:20 +0530

    int example

---
+++ practice/int_example.c
@@ -0,0 +1,10 @@
+// write a programme and prints that age and gpe 
+#include<stdio.h>
+int main()
+{
+    int age =22; // int value
+    float gpa=3.65; // float value
+    printf("MY Age is %d and MY GPA is %.2f",age,gpa); 
+    //.2 meams 2 decimal value place and output show 
+    // print age and age 
+}
