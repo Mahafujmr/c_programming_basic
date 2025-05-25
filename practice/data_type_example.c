36cadf6de45e03db5fba4c70cef687b9396e41d1
Mahafujmr <mrtuhin444@gmail.com>
2025-05-25 21:47:41 +0530

    data type example

---
+++ practice/data_type_example.c
@@ -0,0 +1,15 @@
+/*
+write a programme prints data type example . and use int, float, and char
+
+*/
+#include<stdio.h>
+int main()
+{
+    int age =22;
+    float gpa =3.5900;
+    char ch ='A';
+    printf("My Age is %d\n",age);
+    printf("MY Gpa is %.2f\n",gpa);
+    printf("MY grade is %c\n",ch);
+    return 0; 
+}
