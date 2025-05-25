e93730be402b0def4b1c881be0f55b21261ffc9e
Mahafujmr <mrtuhin444@gmail.com>
2025-05-25 22:41:46 +0530

    example use scanf use

---
+++ practice/scanf_example.c
@@ -0,0 +1,13 @@
+/*
+write a programme prints scanf and output result show
+*/
+#include<stdio.h>
+int main()
+{
+    int age;
+    float result;
+    printf("Enter my Output reslut: ");
+    scanf("%d,%f",&age, &result);
+    printf("Age: %d, Result : %.2f\n",age, result);
+    return 0;
+}
