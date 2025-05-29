1ea2689cac7a0b29e79f7d8ba67f529b9127090a
Mahafujmr <mrtuhin444@gmail.com>
2025-05-30 01:56:02 +0530

    c progamme data type

---
+++ practice/data_type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int roll =10;
+    float gpa =3.59;
+    char grade = 'A';
+    char str[] ="My Result is A+";
+
+    printf("My Roll Number is %d\n",roll);
+    printf("My Gpa is %.2f\n",gpa);
+    printf("My Grade is %c\n",grade);
+    printf("My Feadback is %s\n",str);
+
+}
