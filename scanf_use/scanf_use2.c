d6a8129a6ec4255a65cb930d85192681fc75c662
Mahafujmr <mrtuhin444@gmail.com>
2025-06-01 18:12:05 +0530

    scanf example

---
+++ scanf_use/scanf_use2.c
@@ -0,0 +1,18 @@
+// write a programme scanf and show out put 
+#include<stdio.h>
+int main()
+{
+    // declear variable 
+    int number1, number2;
+    // show first number 
+    printf("Enter the First Number show :");
+    // show sceond number 
+    scanf("%d",&number1);
+    printf("Enter the sceond Number show :");
+    scanf("%d",number2);
+
+    printf(" Number are :   %d  %d\n", number1,number2);
+
+    return 0;
+
+}
