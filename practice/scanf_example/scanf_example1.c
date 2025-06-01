3e4097c648854766a9f5ca9cf0f61c392ccd24f4
Mahafujmr <mrtuhin444@gmail.com>
2025-06-02 01:22:31 +0530

    scanf  example

---
+++ practice/scanf_example/scanf_example1.c
@@ -0,0 +1,8 @@
+// write a programme use scanf and show output
+#include<stdio.h>
+int main(){
+    int number1, number2;
+    printf("Show two number in Console :");
+    scanf(" %d %d",&number1, &number2);
+    printf("There are two number : %d %d\n",number1, number2);
+}
