1af114edd48d49cebbb90659a087a9b3574434bc
Mahafujmr <mrtuhin444@gmail.com>
2025-06-01 17:58:23 +0530

    use scanf

---
+++ scanf_use/scanf_use1.c
@@ -0,0 +1,24 @@
+/// write a programme scanf use 
+#include<stdio.h>
+int main(){
+    /// use int
+    int number;
+    // show screen message 
+    printf("Enter the intege number :");
+    // use scanf, output value show
+    scanf("%d",&number);
+    //print int = d 
+    printf("%d\n",number);
+
+
+    /// use float
+    float grade ;
+    // show screen message 
+    printf("Enter the Floating Number :");
+    // use scanf , output value show this keyword
+    scanf("%f",&grade);
+    // out put print , 
+    printf("%.2f\n",grade);
+
+    return 0;
+}
