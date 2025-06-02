8e4514ee2ffe4c659a1d23ca385d88290f332fa6
Mahafujmr <mrtuhin444@gmail.com>
2025-06-02 12:35:08 +0530

    example scanf

---
+++ scanf_use/scanf_ues3.c
@@ -0,0 +1,10 @@
+// write a programme scanf use and data type int or float
+#include<stdio.h>
+int main(){
+    int number;
+    float result;
+    printf("My Roll Number or Result :");
+    scanf("%d %f", &number , &result);
+    printf("show outpur %d %.2f",number, result);
+    return 0;
+}
