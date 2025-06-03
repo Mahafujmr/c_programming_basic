28f79331693b5825fe576924f284c9f737d32fd3
Mahafujmr <mrtuhin444@gmail.com>
2025-06-04 01:44:35 +0530

    escape sequense example

---
+++ escape_sequence/escape_sequence_example.c
@@ -0,0 +1,18 @@
+/// write a programme escape sequence  an example
+#include<stdio.h>
+int main()
+{
+   // Create Horizontal Tab / or gap = \t
+    printf("Hello I am Tuhin \t I am  Learning C programme\n");
+
+    //Inserts a backslash character (\) = \\ 
+
+  printf("Tuhin And \\");
+  printf("Hello dear\n");
+
+  // Inserts a double quote character \" = \"include character\"
+  printf("Hello My Name is Mahafujur Rahman Tuhin . But My Nick Name is \"Tuhin\" ");
+
+  return 0;
+
+}
---
+++ escape_sequence/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+/// write a programme escape sequence example
+#include<stdio.h>
+int main()
+{
+   // Create Horizontal Tab / or gap = \t
+    printf("Hello I am Tuhin \t I am  Learning C programme\n");
+
+    //Inserts a backslash character (\) = \\ 
+
+  printf("Tuhin And \\");
+  printf("Hello dear\n");
+
+  // Inserts a double quote character \" = \"include character\"
+  printf("Hello My Name is Mahafujur Rahman Tuhin . But My Nick Name is \"Tuhin\" ");
+
+
+}
