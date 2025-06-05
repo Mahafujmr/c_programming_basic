698fb3f04b2ef1b745965f263e8e97f65084704e
Mahafujmr <mrtuhin444@gmail.com>
2025-06-05 23:39:23 +0530

    scanf example

---
+++ practice/scanf_example/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+/// write a programme show our output
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter your Roll Number :");
+    scanf("%d",&number);
+    printf("%d",number);
+}
