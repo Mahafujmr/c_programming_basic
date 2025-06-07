41785289befacd1b6460532d95ba298f5cdf94a7
Mahafujmr <mrtuhin444@gmail.com>
2025-06-07 16:40:47 +0530

    fgets and puts use

--- gets_fgets/fgets_puts.c
+++ gets_fgets/fgets_puts.c
@@ -3,9 +3,13 @@
 #include<stdio.h>
 int main()
 {
+    // declaer charrater array and store homeAdress (max 55 charater)
     char homeAddress [55];
+    // ask user to enter home address
     printf("Enter Your Home Address:");
+    // read the input use fgets
     fgets(homeAddress ,sizeof(homeAddress),stdin);
+    // display show enter homeAddress
     printf("My Home Address Is :");
     puts(homeAddress);
     return 0;
