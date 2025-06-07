6af201056feb20110439cdc8b204ac1f6e9b32bd
Mahafujmr <mrtuhin444@gmail.com>
2025-06-07 11:42:56 +0530

    gets function usecase

---
+++ gets_fgets/gets_example.c
@@ -0,0 +1,10 @@
+// write a programme thats print a gets , fgets, getchar
+#include<stdio.h>
+int main(){
+    char name [50];
+    printf("Enter Your name:");
+    scanf("%s",&name);
+    printf("Hello Dear Friend %s",name);
+    return 0;
+
+}
