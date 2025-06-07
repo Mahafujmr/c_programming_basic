5cdbfb126936259973eb75893670d9521c520715
Mahafujmr <mrtuhin444@gmail.com>
2025-06-07 17:02:53 +0530

    get charater use example

---
+++ gets_fgets/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+// write a programme thats print get charater 
+#include<stdio.h>
+int main()
+{
+    // declare char and variable is lowerCharater
+    char lowerCharater;
+    // show message
+    printf("Enter Your Grade: ");
+    // use get char 
+    /*
+    * first use variable name and then = then getchar use
+    */
+   lowerCharater =getchar();
+   printf("Enter Your char :  ");
+    
+    
+}
