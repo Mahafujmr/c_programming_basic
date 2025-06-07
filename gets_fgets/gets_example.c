759a4ebe2ae414cbe2044ab67c30d95a09bbbfee
Mahafujmr <mrtuhin444@gmail.com>
2025-06-07 11:48:35 +0530

    use gets and puts

--- gets_fgets/gets_example.c
+++ gets_fgets/gets_example.c
@@ -1,10 +1,10 @@
 // write a programme thats print a gets , fgets, getchar
 #include<stdio.h>
 int main(){
-    char name [50];
-    printf("Enter Your name:");
-    scanf("%s",&name);
-    printf("Hello Dear Friend %s",name);
-    return 0;
+    char myName [50];
+    printf("Enter Your name :");
+    gets(myName);
+    printf("My name is ");
+    puts(myName);
 
 }
