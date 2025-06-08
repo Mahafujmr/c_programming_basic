c4be7975f8144a49e973560268c9eea45dc717a1
Mahafujmr <mrtuhin444@gmail.com>
2025-06-08 18:00:41 +0530

    basic problem solution

---
+++ problem_solution/problem_01.c
@@ -0,0 +1,11 @@
+// problem- print name, age, hieght
+#include<stdio.h>
+int main()
+{
+    char name []="MD Tuhin Hossain";
+    int age = 22;
+    float hieght = 5.2;
+    printf("My name is %s, My Age is %d, My Hieght is %.1f inch ",name, age, hieght);
+    return 0 ;
+
+}
