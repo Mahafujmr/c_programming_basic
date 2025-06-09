0ed01dd1742bd5b675044d78afd6cba75980cc76
Mahafujmr <mrtuhin444@gmail.com>
2025-06-09 10:26:26 +0530

    basic problem solution

---
+++ problem_solution/problem_03.c
@@ -0,0 +1,9 @@
+// write a programme print input charater
+#include<stdio.h>
+int main(){
+    char grade ;
+    printf("Enter Your Result Grade :");
+    scanf("%c",&grade);
+    printf("My Result Grade : %c",grade);
+    return 0;
+}
