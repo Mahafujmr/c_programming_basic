6958bcbd70cf075ea4d14a697e31f81f703a1500
Mahafujmr <mrtuhin444@gmail.com>
2025-06-11 00:33:37 +0530

    use sizef and example

---
+++ extra_example/sizef_example.c
@@ -0,0 +1,15 @@
+// find c programme data type memory bytes int, float, double, char
+#include<stdio.h>
+int main()
+{
+    int a ;
+    float b;
+    double c ;
+    char d;
+    // find data type memory bytes so use sizef(variable name);
+    printf("Size of int = %d bytes\n",sizeof(a));
+    printf("Size of float = %d bytes\n",sizeof(b));
+    printf("Size of double = %d bytes\n",sizeof(c));
+    printf("Size of char = %d bytes\n",sizeof(d));
+    
+}
