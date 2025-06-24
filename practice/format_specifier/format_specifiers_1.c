8d36c4f90ba234bda17d85c081022a9aa8fa35d9
Mahafujmr <mrtuhin444@gmail.com>
2025-06-24 15:20:25 +0530

    format specifiers

---
+++ practice/format_specifier/format_specifiers_1.c
@@ -0,0 +1,26 @@
+// write a programme format specifieres 
+#include<stdio.h>
+int main()
+{
+    int number =10;
+    int num = 222;
+    float doubeNum = 35.00;
+    char idNo= 'A';
+    printf("Integer Number = %d \n",num);
+    printf("Floating Number = %.2f \n",doubeNum);
+    printf("My Text is  = %c\n",idNo);
+
+    // other 
+    printf("My Intger Number is %d,My Floating Number is %f,and My Char is %c \n",num,doubeNum,idNo);
+
+    printf("My Favourite Number is %d \n",5);
+    printf("My Favourite Letter is %c \n",'M');
+    printf("My Result is %f \n",3.59);
+
+    printf("%d \n",number); // int = %d
+
+
+
+
+    return 0;
+}
