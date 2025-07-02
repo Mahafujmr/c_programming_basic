f4bb6f9e1749f8d368e87f5e10396360e4cdcfdd
Mahafujmr <mrtuhin444@gmail.com>
2025-07-02 20:33:59 +0800

    topic arithmetic operator

---
+++ operator/arithmetic_operator/arithmetic_operator.c
@@ -0,0 +1,13 @@
+// write a problem print 3 number addtion 
+#include<stdio.h>
+int main()
+{
+
+    // declare varibale
+    int a,b,c;
+    // value include variable
+     a=b=c = 100;
+      int add = a=b=c;
+     printf("%d",add);
+    return 0;
+}
