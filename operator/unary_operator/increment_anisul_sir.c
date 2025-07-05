c0e627d1bd4a0b31ffb4ed4db5b8c4b4f11881a3
Mahafujmr <mrtuhin444@gmail.com>
2025-07-06 04:00:05 +0800

    unary operator problem solution

--- operator/unary_operator/increment_anisul_sir.c
+++ operator/unary_operator/increment_anisul_sir.c
@@ -12,7 +12,19 @@ int main()
    int m = 10;
   int n = ++m;
   printf("m = %d\n",m);
-  printf("n = %d",n);
+  printf("n = %d\n",n);
+
+    printf("Decrement\n");
+     int t = 10;
+  int v = t--;
+  printf("t = %d\n",t); //t= 9
+  printf("v = %d\n",v);
+
+   printf("Decrement\n");
+     int o = 12;
+  int p = --o;
+  printf("o = %d\n",o); //0 =11
+  printf("p = %d\n",p);
 
     return 0 ;
 }
