1af9f24e2f9b36e0da42b63845baabb753f9c73e
Mahafujmr <mrtuhin444@gmail.com>
2025-07-06 04:40:29 +0800

    Another problem solve relational operator

---
+++ operator/Relational_operator/problem_02_anisul_sir.c
@@ -0,0 +1,17 @@
+// write a  programme a if else
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Enter The Number:");
+  scanf("%d",&num);
+  if (num %2 == 0)
+  {
+    printf("This Number is Even");
+  }
+  else
+  printf("This number is ODD");
+  
+
+    return 0;
+}
