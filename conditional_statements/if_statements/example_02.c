53814c329592f6de0e3b4dec2c0881a7c2df6162
Mahafujmr <mrtuhin444@gmail.com>
2025-07-10 01:09:57 +0530

    conditional if statements

---
+++ conditional_statements/if_statements/example_02.c
@@ -0,0 +1,18 @@
+// write a programme and input value then check voter 
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter Your age:");
+    scanf("%d",&age);
+    if (age > 18)
+    {
+      printf("You are voter");
+    }
+    else
+    {
+   printf("You are Note Voter");
+    }
+ 
+    
+}
