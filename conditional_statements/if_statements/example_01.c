711174dbfd36d688cf14bdd785e54ac5d1b9e9ff
Mahafujmr <mrtuhin444@gmail.com>
2025-07-10 01:05:39 +0530

    conditonal statment

---
+++ conditional_statements/if_statements/example_01.c
@@ -0,0 +1,13 @@
+// write a programme if statment and print result
+#include<stdio.h>
+int main()
+{
+    int age = 22;
+    // use if statement 
+    // if statement conditon is - only one conditons check
+    if (age >18)
+    {
+        printf("You are Voter");
+    }
+    
+}
