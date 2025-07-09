df863b307c25d6fe0df1c25daecb64c4805613da
Mahafujmr <mrtuhin444@gmail.com>
2025-07-10 01:18:40 +0530

    anothe example if statement

---
+++ conditional_statements/if_statements/example_03.c
@@ -0,0 +1,17 @@
+// write a programme check your age adult
+#include<stdio.h>
+int main()
+{
+    // declare variable
+    int yourAge ;
+    // use printf function 
+    printf("Enter Your age:");
+    // use scanf - input use console
+    scanf("%d",&yourAge);
+    //use conditional statements- if statements
+    if (yourAge >= 20)
+    {
+        printf("You are Adult");
+    }
+    
+}
