c633a2601655bb8a2c5ff70b8eba5d49eb4bed97
Mahafujmr <mrtuhin444@gmail.com>
2025-07-11 23:24:17 +0530

    if statement

---
+++ conditional_statements/if_statements/example_04.c
@@ -0,0 +1,11 @@
+// write a programme if conditonal statements
+#include<stdio.h>
+int main()
+{
+    int number= 100;
+    if (number > 80)
+    {
+        printf("This People is good");
+    }
+    
+}
