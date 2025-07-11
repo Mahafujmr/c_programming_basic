076b857eaa80a745314822911f62cdab5344a6da
Mahafujmr <mrtuhin444@gmail.com>
2025-07-11 23:26:30 +0530

    if statement

--- conditional_statements/if_statements/example_04.c
+++ conditional_statements/if_statements/example_04.c
@@ -2,9 +2,11 @@
 #include<stdio.h>
 int main()
 {
+    // number of people
     int number= 100;
     if (number > 80)
     {
+        // conditonal statement inside decision making statement
         printf("This People is good");
     }
     
