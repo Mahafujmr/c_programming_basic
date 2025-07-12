6e877af3673445f5edb748313f6efcead204e2f4
Mahafujmr <mrtuhin444@gmail.com>
2025-07-13 02:54:15 +0530

    switch statement

--- conditional_statements/switch_statement/example_01.c
+++ conditional_statements/switch_statement/example_01.c
@@ -2,6 +2,25 @@
 #include<stdio.h>
 int main()
 {
-    
-    return 0;
+    // variable store the day number
+    int day = 3;
+    // switch statement check the day value
+    switch(day){
+        // exit switch check this block then next
+        case 1:
+        printf("This is Saturday");
+        break;
+        case 2:
+        printf("This is Sunday");
+        break;
+        case 3:
+        printf("This is Monday");
+        break; 
+        case 4:
+        printf("This is Tuesday");
+        // last one no case matches then show default case 
+        default :
+        printf("This is Default Day");
+    }
+    return 0;// successfully termination this programme
 }
