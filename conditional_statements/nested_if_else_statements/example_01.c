597295faf53aaf8b7ff33e5ff2683d2a7bd6211d
Mahafujmr <mrtuhin444@gmail.com>
2025-07-12 23:36:20 +0530

    nested if else

---
+++ conditional_statements/nested_if_else_statements/example_01.c
@@ -0,0 +1,35 @@
+// write a programme nested if else statement
+#include<stdio.h>
+int main()
+{
+    int age =17;
+    if (age > 19)
+    {
+        if (age >19)
+        {
+           printf("I am voter");
+        }
+        else
+        {
+            printf("I am not voter");
+        }
+        
+        
+    }
+    else
+    {
+        if (age >19)
+        {
+            printf("I am Teacher");
+        }
+        else
+        {
+            printf("I am not teacher");
+        }
+        
+        
+    }
+    
+    
+    return 0;
+}
