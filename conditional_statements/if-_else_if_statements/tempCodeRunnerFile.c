201ce49a0de3a3d0546372b02740a06fe86a7693
Mahafujmr <mrtuhin444@gmail.com>
2025-07-12 23:26:52 +0530

    if else if statements

---
+++ conditional_statements/if-_else_if_statements/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+// write a programme if else if statements
+#include<stdio.h>
+int main()
+{
+    // use if else if statement
+    int age = 22;
+    if (age == 20)
+    {
+        printf("I am student");
+    }
+    else if (age == 15)
+    {
+        printf("I am not student");
+    }
+    else if (age == 22)
+    {
+        printf("I am a teacher");
+    }
+    else{
+        printf("I am a Player");
+    }
+    
+    
+    
+}
