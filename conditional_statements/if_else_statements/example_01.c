0204df910b100803cbd342e2fb5b32f2fd15ae26
Mahafujmr <mrtuhin444@gmail.com>
2025-07-12 22:40:50 +0530

    if else statements

---
+++ conditional_statements/if_else_statements/example_01.c
@@ -0,0 +1,16 @@
+// write a programme if else statements
+#include<stdio.h>
+int main()
+{
+    int peopleAge = 18;
+    // use if else statements for conditaional statements
+    if (peopleAge>15)
+    {
+        printf("Eligible for vote");
+    }
+    else{
+        printf("Not Eligible for vote");
+    }
+    
+    return 0;
+}
