3d257bdb8e8f045a75aa40be19fa3e099d2185fd
Mahafujmr <mrtuhin444@gmail.com>
2025-07-17 01:11:21 +0530

    for loop

---
+++ loop_statements/for_loop/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+//write a programme for loop
+#include<stdio.h>
+int main()
+{
+    int i;// variable declaration
+
+    // for loop structure
+    // i =1 --> variable initialization
+    // i <= 5 --> condition
+    // i++ --> counter Update
+    // { body } --> body
+    // variable increment or decrement part --> counter update
+    for(i=1; i <= 5; i++)
+
+
+    printf("C programming language\n");
+    return 0;
+}
