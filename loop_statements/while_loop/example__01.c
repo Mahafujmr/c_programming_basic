4bd45800da8c7832c82d61aaea56ab50decd1433
Mahafujmr <mrtuhin444@gmail.com>
2025-07-17 14:34:11 +0530

    while loop

---
+++ loop_statements/while_loop/example__01.c
@@ -0,0 +1,22 @@
+// write a programme while loop
+// => While loop syntax
+/*
+first => variable initilization ;
+second => while (conditon)
+{
+printf();
+third => increment and decrement ;
+}
+*/
+#include<stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("This Number is 10\n");
+        i++;
+    }
+    
+
+}
