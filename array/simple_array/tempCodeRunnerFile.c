278ac8301c892700bbb501f99a24f909703aad3f
Mahafujmr <mrtuhin444@gmail.com>
2025-07-22 00:14:42 +0530

    array problem solution

--- array/simple_array/tempCodeRunnerFile.c
+++ array/simple_array/tempCodeRunnerFile.c
@@ -1,10 +1,16 @@
 // write a programme array for loop
+/*
+- variable declaration
+-  use for loop(variable initialization,condition,increment)
+- instraction use printf
+- use scanf - input terminal
+*/
 #include<stdio.h>
 int main()
 {
     int num [5];
     for(int index=0; index<5;index++){
-        printf("Number  %d : ",index);
+        printf("Number  %d : ",index+1);
         scanf("%d",&num[index]);
     }
     return 0;
